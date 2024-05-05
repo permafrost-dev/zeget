cba41b238c5c32496a49996ee90c4e61a5ab6067
Patrick Organ <patrick@permafrost.dev>
2024-05-05 10:39:46 -0400

    refactoring

--- lib/home/home.go
+++ lib/home/home.go
@@ -64,42 +64,9 @@ func (pe *PathExpander) Expand(path string) (string, error) {
 	return strings.Replace(path, pe.makePath("~"), pe.makePath(homeString), 1), nil
 }
 
-// func Home() (string, error) {
-// 	userData, err := user.Current()
-// 	if err != nil {
-// 		return "", fmt.Errorf("find homedir: %w", err)
-// 	}
-// 	return userData.HomeDir, err
-// }
-
 // Expand takes a path as input and replaces ~ at the start of the path with the user's
 // home directory. Does nothing if the path does not start with '~'.
 func Expand(path string, homeDirectoryPath ...string) (string, error) {
 	pe := NewPathExpander(homeDirectoryPath[0])
 	return pe.Expand(path)
 }
-
-// 	if !strings.HasPrefix(path, "~") {
-// 		return path, nil
-// 	}
-
-// 	var userData *user.User
-// 	var err error
-
-// 	homeString := strings.Split(filepath.ToSlash(path), "/")[0]
-// 	if homeString == "~" {
-// 		userData, err = user.Current()
-// 		if err != nil {
-// 			return "", fmt.Errorf("expand tilde: %w", err)
-// 		}
-// 	} else {
-// 		userData, err = user.Lookup(homeString[1:])
-// 		if err != nil {
-// 			return "", fmt.Errorf("expand tilde: %w", err)
-// 		}
-// 	}
-
-// 	home := utilities.SetIf(len(homeDirectoryPath) == 0, homeDirectoryPath[0], userData.HomeDir)
-
-// 	return strings.Replace(path, homeString, home, 1), nil
-// }
