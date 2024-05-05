d2bdcea80162058bb0de17265cde7964e4996ac0
Patrick Organ <patrick@permafrost.dev>
2024-05-05 12:44:45 -0400

    wip

--- lib/finders/direct_asset_finder_test.go
+++ lib/finders/direct_asset_finder_test.go
@@ -1,22 +1,56 @@
 package finders_test
 
 import (
+	"bytes"
+	"io"
+	"net/http"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/permafrost-dev/eget/lib/assets"
-	"github.com/permafrost-dev/eget/lib/download"
 	. "github.com/permafrost-dev/eget/lib/finders"
 )
+
+type MockHTTPRequestData struct {
+	Method string
+	URL    string
+}
+
+type MockHTTPClient struct {
+	Requests []MockHTTPRequestData
+	DoFunc   func(req *http.Request) (*http.Response, error)
+}
+
+func (m *MockHTTPClient) RoundTrip(req *http.Request) (*http.Response, error) {
+	return newMockResponse("mock body", http.StatusOK), nil
+}
+
+func (m *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	m.Requests = append(m.Requests, MockHTTPRequestData{Method: req.Method, URL: req.URL.String()})
+	return m.DoFunc(req)
+}
+
+// Utility function to create a mock HTTP response
+func newMockResponse(body string, statusCode int) *http.Response {
+	return &http.Response{
+		StatusCode: statusCode,
+		Body:       io.NopCloser(bytes.NewBufferString(body)),
+	}
+}
 
 var _ = Describe("DirectAssetFinder", func() {
 	var (
 		directAssetFinder *DirectAssetFinder
-		downloadClient    *download.Client
+		//downloadClient    interface{}
 	)
 
 	BeforeEach(func() {
 		directAssetFinder = &DirectAssetFinder{URL: "https://example.com/asset.zip"}
-		downloadClient = &download.Client{} // Assuming no initialization parameters for simplicity
+		// downloadClient = &MockHTTPClient{
+		// 	DoFunc: func(req *http.Request) (*http.Response, error) {
+		// 		return newMockResponse("mock body", http.StatusOK), nil
+		// 	},
+		// }
 	})
 
 	Describe("Find", func() {
@@ -26,7 +60,7 @@ var _ = Describe("DirectAssetFinder", func() {
 				DownloadURL: "https://example.com/asset.zip",
 			}
 
-			assets, err := directAssetFinder.Find(downloadClient)
+			assets, err := directAssetFinder.Find(nil)
 
 			Expect(err).To(BeNil())
 			Expect(assets).To(HaveLen(1))
