bf4fa36454d262dfdca77b97c751fb0fa9dc4519
Patrick Organ <patrick@permafrost.dev>
2024-05-13 17:28:08 -0400

    add unit tests, test fixtures, wip

---
+++ lib/extraction/extractors_test.go
@@ -0,0 +1,77 @@
+package extraction_test
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/permafrost-dev/eget/lib/extraction"
+	"github.com/twpayne/go-vfs/v5"
+	"github.com/twpayne/go-vfs/v5/vfst"
+)
+
+type MockChooser struct {
+	ChooseFn func(name string, dir bool, mode fs.FileMode) (direct bool, possible bool)
+}
+
+func (mc *MockChooser) Choose(name string, dir bool, mode fs.FileMode) (direct bool, possible bool) {
+	if mc.ChooseFn != nil {
+		return mc.ChooseFn(name, dir, mode)
+	}
+	return false, false
+}
+
+var gunzipper = func(r io.Reader) (io.Reader, error) {
+	return gzip.NewReader(r)
+}
+
+var _ = Describe("Extractor", func() {
+	var testFS *vfst.TestFS
+	var cleanup func()
+	var err error
+
+	BeforeEach(func() {
+		testFS, cleanup, err = vfst.NewTestFS(nil)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		cleanup()
+	})
+
+	// var extractor *extraction.SingleFileExtractor
+
+	Describe("NewExtractor", func() {
+		It("should create an ArchiveExtractor for .tar.gz files", func() {
+			extractor := extraction.NewExtractor(testFS, "test.tar.gz", "", nil)
+			Expect(extractor).To(BeAssignableToTypeOf(&extraction.ArchiveExtractor{}))
+		})
+
+		It("should create a SingleFileExtractor for .gz files", func() {
+			extractor := extraction.NewExtractor(testFS, "test.gz", "", nil)
+			Expect(extractor).To(BeAssignableToTypeOf(&extraction.SingleFileExtractor{}))
+		})
+	})
+
+	Describe("SingleFileExtractor", func() {
+		Context("when extracting a gzip file", func() {
+			It("should successfully extract the content", func() {
+				wd, _ := os.Getwd()
+				testArchiveFn := fmt.Sprintf("test archive: %s/../../test/test-config-toml.gz", wd)
+				buf, err := os.ReadFile(testArchiveFn)
+
+				extractor := extraction.NewExtractor(vfs.OSFS, testArchiveFn, "test-config-toml", nil)
+				ef, _, err := extractor.Extract(buf, false)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ef.Name).To(Equal("test-config-toml"))
+
+				defer ef.Extract(ef.Name)
+			})
+		})
+
+	})
+})
