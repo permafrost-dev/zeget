c56f7aab1c1b17437c10266fbc7f1f73fab92926
Patrick Organ <patrick@permafrost.dev>
2024-05-13 16:34:38 -0400

    add unit tests, wip

---
+++ lib/filters/filters_test.go
@@ -0,0 +1,34 @@
+package filters_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	. "github.com/permafrost-dev/eget/lib/filters"
+)
+
+var _ = Describe("Filters", func() {
+	Describe("FilenameToAssetFilters", func() {
+		It("should return guessed filters for filenames without known patterns", func() {
+			Expect(FilenameToAssetFilters("random-file")).To(Equal([]string{"file"}))
+		})
+
+		It("should return correct filters for a filename with known patterns", func() {
+			filename := "test-amd64.exe"
+			expectedFilters := []string{"amd64", "exe"}
+			Expect(FilenameToAssetFilters(filename)).To(Equal(expectedFilters))
+		})
+
+		It("should handle multiple occurrences of the same architecture", func() {
+			filename := "test-amd64-amd64"
+			expectedFilters := []string{"amd64", "amd64"}
+			Expect(FilenameToAssetFilters(filename)).To(ConsistOf(expectedFilters))
+		})
+
+		It("should include unknowns", func() {
+			filename := "test-linux-unknown-amd64.exe"
+			expectedFilters := []string{"linux", "unknown", "amd64", "exe"}
+			Expect(FilenameToAssetFilters(filename)).To(Equal(expectedFilters))
+		})
+	})
+})
