86b0d9a609f9864a97e98709c74c5c18f4c99cc9
Patrick Organ <patinthehat@users.noreply.github.com>
2024-05-14 05:15:11 -0400

    Update update-readme-hash.sh

--- scripts/update-readme-hash.sh
+++ scripts/update-readme-hash.sh
@@ -7,5 +7,4 @@ if [ -d ".git" ]; then
 fi
 
 NEWHASH=$(sha256sum $INSTALLER_FN | awk '{print $1}')
-cat README.md | sed "s|<code data-id=\"installer-checksum\">.*</code><br>|<code data-id=\"installer-checksum\">$NEWHASH</code><br>|g" > README.md.tmp
-mv -f README.md.tmp README.md
+sed "s|<code data-id=\"installer-checksum\">.*</code><br>|<code data-id=\"installer-checksum\">$NEWHASH</code><br>|g" --in-place
