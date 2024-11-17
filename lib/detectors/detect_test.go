4e30e467ab75e1eac5b6c085cb2690b9d2263a51
Patrick Organ <patrick@permafrost.dev>
2024-11-17 15:26:00 -0500

    improve detector unit tests

--- lib/detectors/detect_test.go
+++ lib/detectors/detect_test.go
@@ -1,7 +1,9 @@
 package detectors_test
 
 import (
+	"fmt"
 	"reflect"
+	"regexp"
 	"runtime"
 	"testing"
 
@@ -198,18 +200,20 @@ func TestSystemDetector_Detect(t *testing.T) {
 
 func TestDetermineCorrectDetector(t *testing.T) {
 	tests := []struct {
-		name    string
-		flags   appflags.Flags
-		want    Detector
-		wantErr bool
+		name       string
+		flags      appflags.Flags
+		want       Detector
+		wantErr    bool
+		wantAssets []string
 	}{
 		{
 			name: "all detector",
 			flags: appflags.Flags{
 				System: "all",
 			},
-			want:    &AllDetector{},
-			wantErr: false,
+			want:       &AllDetector{},
+			wantErr:    false,
+			wantAssets: []string{},
 		},
 		{
 			name: "specific system detector",
@@ -220,7 +224,8 @@ func TestDetermineCorrectDetector(t *testing.T) {
 				Os:   OSLinux,
 				Arch: ArchAMD64,
 			},
-			wantErr: false,
+			wantErr:    false,
+			wantAssets: []string{},
 		},
 		{
 			name:  "default system detector",
@@ -231,7 +236,8 @@ func TestDetermineCorrectDetector(t *testing.T) {
 				Os:   OSLinux,
 				Arch: ArchAMD64,
 			},
-			wantErr: false,
+			wantErr:    false,
+			wantAssets: []string{},
 		},
 		{
 			name: "invalid system format",
@@ -242,7 +248,33 @@ func TestDetermineCorrectDetector(t *testing.T) {
 				Os:   OSLinux,
 				Arch: ArchAMD64,
 			},
-			wantErr: false,
+			wantErr:    false,
+			wantAssets: []string{},
+		},
+		{
+			name: "test 3",
+			flags: appflags.Flags{
+				System: "linux/amd64",
+				Asset:  []string{"asset1", "^asset2"},
+			},
+			want: &DetectorChain{
+				Detectors: []Detector{
+					&SingleAssetDetector{
+						Asset:     "asset1",
+						Anti:      false,
+						IsPattern: false,
+						Compiled:  nil,
+					},
+					&SingleAssetDetector{
+						Asset:     "asset2",
+						Anti:      true,
+						IsPattern: true,
+						Compiled:  regexp.MustCompile("^asset2"),
+					},
+				},
+			},
+			wantErr:    false,
+			wantAssets: []string{},
 		},
 	}
 
@@ -250,6 +282,7 @@ func TestDetermineCorrectDetector(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			system, _ := NewSystemDetector(runtime.GOOS, runtime.GOARCH)
 			got, err := DetermineCorrectDetector(&tt.flags, []string{}, system)
+			fmt.Printf("got: %v\n", got)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("DetermineCorrectDetector() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -258,5 +291,65 @@ func TestDetermineCorrectDetector(t *testing.T) {
 				t.Errorf("DetermineCorrectDetector() got = %T, want %T", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestGetPatternDetectors(t *testing.T) {
+	ignoredPatterns := []string{
+		"^foo$",
+		"bar.*",
+		"baz[0-9]+",
+	}
+
+	systemDetector := &SystemDetector{
+		Os:   OSLinux,
+		Arch: ArchAMD64,
+	}
+
+	detectorChain, err := GetPatternDetectors(ignoredPatterns, systemDetector)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if detectorChain == nil {
+		t.Fatal("Expected detectorChain to be non-nil")
+	}
+
+	expectedLength := len(ignoredPatterns)
+	if len(detectorChain.Detectors) != expectedLength {
+		t.Fatalf("Expected %d detectors, got %d", expectedLength, len(detectorChain.Detectors))
+	}
+
+	for i, detector := range detectorChain.Detectors {
+		singleAssetDetector, ok := detector.(*SingleAssetDetector)
+		if !ok {
+			t.Fatalf("Detector at index %d is not a *SingleAssetDetector", i)
+		}
+
+		expectedPattern := ignoredPatterns[i]
+
+		if singleAssetDetector.Asset != expectedPattern {
+			t.Errorf("Detector at index %d has Asset %q, expected %q", i, singleAssetDetector.Asset, expectedPattern)
+		}
+
+		if !singleAssetDetector.Anti {
+			t.Errorf("Detector at index %d has Anti %v, expected true", i, singleAssetDetector.Anti)
+		}
+
+		if !singleAssetDetector.IsPattern {
+			t.Errorf("Detector at index %d has IsPattern %v, expected true", i, singleAssetDetector.IsPattern)
+		}
+
+		if singleAssetDetector.Compiled == nil {
+			t.Errorf("Detector at index %d has nil Compiled regexp", i)
+		} else {
+			if singleAssetDetector.Compiled.String() != expectedPattern {
+				t.Errorf("Detector at index %d has Compiled regexp %q, expected %q", i, singleAssetDetector.Compiled.String(), expectedPattern)
+			}
+		}
+	}
+
+	if detectorChain.System != systemDetector {
+		t.Errorf("Expected detectorChain.System to be the same as input systemDetector")
 	}
 }
